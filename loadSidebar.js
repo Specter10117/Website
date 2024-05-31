75d3b53a09d502fcf0a80beee5b0897f2308cdb8
Specty <spectytvbusiness@gmail.com>
2024-05-31 07:09:32 -0400

    Update loadSidebar.js

--- loadSidebar.js
+++ loadSidebar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('sidebar.html')
+    fetch('/sidebar.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
